1430d3de4718adfe5868ab78fd8538d49272e182
周艺 <me@ichou.cn>
2016-07-29 11:08:02 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,44 @@
+# Trace (Proxy-helper).alfredworkflow
+======================
+
+在 Mac 上切换代理是一件麻烦的事情，然而不幸的是一旦你有了这个需求往往也意味着你需要频繁进行这个操作
+
+于是我造了这个偷懒用的 alfredworkflow
+
+希望有人能用得着
+
+### Quickly start
+
+1. Download
+    
+    https://github.com/IvanChou/trace.alfredworkflow/raw/master/Trace(Proxy-helper).alfredworkflow
+    
+2. Setting
+    
+    - Doubule click `trace` in Alfred Workflows Preferences
+    - Click `Open workflow folder` to open Finder
+    - Open `proxy.conf` and modify like sample
+
+3. Use
+    
+    Call out your alfred, and type `trace`
+    
+    ![](http://ww4.sinaimg.cn/mw690/006pIUL1gw1f69r4xsjf0j30g10790tq.jpg)
+    
+4. Enjoy it!
+
+### More feature
+
+When you change your proxy setting via Trace, OSX may ask your password to allow this operation everytime. There is no better way to slove the question, but let trace keep your password.
+
+Open `trace.sh` , set your password
+
+```bash
+# set your password, avoid to be asked every time.
+PWD='yourpwd'
+```
+It's unsafe, but quite convenient. By the way, if nobody has been told, is there anyone know your password has been shown here?
+
+### About Trace
+
+It's inspired by Fate.  "Trace on!"
---
+++ trace.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# Source the workflow library
+. workflowHandler.sh
+
+# set your password, avoid to be asked every time.
+PWD=''
+
+# Config file
+PROFILE_CONF='proxy.conf'
+
+# Icons folder
+ICONS_DIR='./icons'
+
+# Current device
+DEVICE=''
+
+# ======== private ========
+function get_curr_proxy_url()
+{
+    networksetup -getautoproxyurl $DEVICE | grep URL | awk '{print $2}'
+}
+
+function get_curr_socks_server()
+{
+    networksetup -getsocksfirewallproxy $DEVICE | grep Server | awk '{print $2}'
+}
+
+function get_curr_socks_port()
+{
+    networksetup -getsocksfirewallproxy $DEVICE | grep Port | awk '{print $2}'
+}
+
+function get_curr_proxy_state()
+{
+    networksetup -getautoproxyurl $DEVICE | grep Enabled | awk '{print $2}'
+}
+
+function get_curr_socks_state()
+{
+    networksetup -getsocksfirewallproxy $DEVICE | grep Enabled | awk '{print $2}'
+}
+
+function get_curr_proxy_setting()
+{
+    if [[ $(get_curr_proxy_state) = 'Yes' ]]; then
+        proxy_setting=$(get_curr_proxy_url)
+    elif [[ $(get_curr_socks_state) = 'Yes' ]]; then
+        proxy_setting="$(get_curr_socks_server):$(get_curr_socks_port)"
+    else
+        proxy_setting=''
+    fi
+    
+    echo $proxy_setting
+}
+# ======== private ========
+
+# Generate feedback results
+function list_proxy()
+{
+    local cnt=0
+    local curr_setting=$(get_curr_proxy_setting)
+    local title subtitle arg icon
+    
+    while IFS=' ' read proxy_name proxy_setting; do
+        uid="trace-$cnt"
+        let cnt+=1
+    
+        icon="$ICONS_DIR/$proxy_name.png"
+    
+        # Use default workflow icon if not found
+        if [ ! -f "$icon" ]; then
+            icon="icon.png"
+        fi
+    
+        if [[ $curr_setting = $proxy_setting ]]; then
+            title="Now: $proxy_name"
+        else
+            title="$proxy_name"
+        fi
+        
+        if [ ${proxy_setting:0:4} = 'http' ]; then
+            title="$title [AutoProxy]"
+        else
+            title="$title"
+        fi
+
+        subtitle="$proxy_setting"
+        arg="$proxy_name $proxy_setting"
+
+        addResult "$uid" "$arg" "$title" "$subtitle" "$icon" "yes"
+    done <<EOF
+`grep -vE '^#.*' $PROFILE_CONF`
+off Close all proxy
+EOF
+    
+    # Show feedback results
+    getXMLResults
+}
+
+# Turn on the choice proxy
+function on()
+{
+    if [ ${2:0:4} = 'http' ]; then
+        echo $PWD | sudo -S networksetup -setsocksfirewallproxystate $DEVICE off
+        sudo -S networksetup -setautoproxyurl $DEVICE $2
+        echo "$1 AutoProxy, TRACE ON"
+    else
+        domain=$(echo "$2" | awk -F: '{print $1}')
+        port=$(echo "$2" | awk -F: '{print $2}')
+        
+        if [[ $domain != '' ]]&&[[ $port != '' ]]; then
+            echo $PWD | sudo -S networksetup -setautoproxystate $DEVICE off
+            sudo -S networksetup -setsocksfirewallproxy $DEVICE $domain $port
+            echo "$1, TRACE ON！"
+        else
+            echo "TRACE FAILED"
+        fi
+    fi
+}
+
+# Turn off all proxy
+function off()
+{
+    echo $PWD | sudo -S networksetup -setautoproxystate $DEVICE off
+    sudo -S networksetup -setsocksfirewallproxystate $DEVICE off
+    echo "TRACE END"
+}
+
+# The main entry
+function main()
+{
+    local device=$(networksetup -listnetworkserviceorder | awk '
+        BEGIN {
+            "netstat -rn | grep default " | getline var
+            split(var, ARRAY, " ")
+        }
+
+        { if ($5 ~ ARRAY[6]) { NAME=$3 } }
+
+        END { print NAME }
+    ')
+    
+    DEVICE=${device%,*}
+    
+    case $1 in
+        list_proxy)
+        list_proxy $2
+        ;;
+        
+        off)
+        off
+        ;;
+        
+        *)
+        on "$1" "$2"
+        ;;
+    esac
+}
+
+main "$@" # Run from here
